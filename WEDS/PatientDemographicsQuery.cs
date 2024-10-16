da894da85c1201731ac72b6994583ba851e4c7cd
Tim Lucas <74425656+timlucasss@users.noreply.github.com>
2024-10-16 10:53:54 +0100

    Changes

---
+++ WEDS/PatientDemographicsQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace WEDSAPI
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PatientDemographicsQuery
+    {
+        [Required]
+        public string NhsNumber { get; set; }
+
+
+        public PatientDemographicsQuery(string nhsNumber)
+        {
+            NhsNumber = nhsNumber;
+        }
+    }
+}
